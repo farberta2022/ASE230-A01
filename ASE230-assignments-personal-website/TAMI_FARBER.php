fbd3a41da4a39848dba8400ea0935ee268abb0ca
Tami Farber <38335128+farberta2022@users.noreply.github.com>
2022-09-01 23:39:54 -0400

    Add files via upload
    Files altered per instructions

---
+++ ASE230-assignments-personal-website/TAMI_FARBER.php
@@ -0,0 +1,100 @@
+<html lang="en">
+	<!-- https://www.bootdey.com/snippets/view/team-user-resume#html -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+
+
+	<body>
+		<link rel="stylesheet" href="assets/css/detail.css" />
+		<title><?php echo  "ASE 230 - Tami Farber"; ?></title>
+		<div class="container text-center mb-5">
+			<h1><?php echo "This is ASE 230 - Tami Farber";?></h1>
+		</div>
+		<div class="container text-right">
+			<!-- <input class="btn btn-primary" type="return" value="Return"> *-->
+			<a href="index.php" class="btn btn-primary" tabindex="" role="button" aria-disabled="true">Back</a>
+
+		</div>
+
+		<div class="container">
+    <div class="row">
+      <div class="col-lg-5 col-md-6">
+        <div class="mb-2">
+          <img class="w-100" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="">
+        </div>
+        <div class="mb-2 d-flex">
+          <h4 class="font-weight-normal"><?php echo "Tami Farber"; ?></h4>
+          <div class="social d-flex ml-auto">
+            <p class="pr-2 font-weight-normal">Follow on:</p>
+            <a href="#" class="text-muted mr-1">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="text-muted mr-1">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="text-muted mr-1">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="#" class="text-muted">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+        <div class="mb-2">
+          <ul class="list-unstyled">
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">Dream profession:</span>
+              <label class="media-body"><?php echo "Data Analyst"; ?></label>
+            </li>
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">Dream company: </span>
+              <label class="media-body"><?php echo "NASA"; ?></label>
+            </li>
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">Email: </span>
+              <label class="media-body"><?php echo "farbert1@nku.edu"; ?></label>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-7 col-md-6 pl-xl-3">
+        <h5 class="font-weight-normal">Short intro</h5>
+        <p><?php echo "As a student, I have explored math, statistics, chemistry, biology, programming, and algorithms. Mastering these tools and using them to help us learn more about our world would be gratifying. For me, the best future is one where I get to be a part of the movement to heal our planet, so future generations can look to the stars simply for the wonder of discovery."; ?></p>
+        <div class="my-2 bg-light p-2">
+          <p class="font-italic mb-0"><?php echo "I don't know anything with certainty, but seeing the stars makes me dream. <br> -- Vincent Van Gogh";?></p>
+        </div>
+        <div class="mb-2 mt-2 pt-1">
+          <h5 class="font-weight-normal">Top skills</h5>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">Ecological Research</div>
+              <span class="progress-bar-number">70%</span>
+            </div>
+          </div>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">Information Technologies</div>
+              <span class="progress-bar-number">75%</span>
+            </div>
+          </div>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">Education</div>
+              <span class="progress-bar-number">85%</span>
+            </div>
+          </div>
+        </div>
+        <h5 class="font-weight-normal">Fun fact</h5>
+        <p><?php echo "I have been hugging trees for longer than 'Tree-hugger' has been in the common vernacular.";?></p>
+      </div>
+    </div>
+  </div>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
